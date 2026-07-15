bde67e809498c8c3e1c22c0d116e29a85955a505
Dmitriy Ponomarev <dmitro.p.vladimirovich@gmail.com>
2026-07-15 21:16:07 +0300

    driver option headless

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
 from pages.customer_login import CustomerLogin
 from pages.shops_page import ShopsPage
@@ -6,7 +7,9 @@ from pages.shops_page import ShopsPage
 
 @pytest.fixture()
 def driver():
-    chrome_driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    chrome_driver = webdriver.Chrome(options=options)
     chrome_driver.maximize_window()
     yield chrome_driver
     chrome_driver.quit()
