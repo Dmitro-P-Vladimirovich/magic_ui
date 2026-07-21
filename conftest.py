547ac228cbe382bc09288b36b252783e4880c852
Dmitriy Ponomarev <dmitro.p.vladimirovich@gmail.com>
2026-07-21 23:45:52 +0300

    window options

--- conftest.py
+++ conftest.py
@@ -14,12 +14,12 @@ def driver():
     options.add_argument('--no-sandbox')  # Обязательно для запуска от root/SYSTEM
     options.add_argument('--disable-dev-shm-usage')  # Обязательно для Docker (обходит нехватку памяти /dev/shm)
     options.add_argument('--disable-gpu')  # Дополнительная стабильность для headless
+    options.add_argument('window-size=1920,1080')
     chrome_driver = webdriver.Chrome(options=options)
     # chrome_driver.maximize_window()
-    # chrome_driver.implicitly_wait(5)
-    return chrome_driver
-    # filename = f'{str(random.randint(100, 10000))}.png'
-    # chrome_driver.save_screenshot(filename)
+    yield chrome_driver
+    filename = f'{str(random.randint(100, 10000))}.png'
+    chrome_driver.save_screenshot(filename)
     # allure.attach(chrome_driver.get_screenshot_as_png(), name="Screenshot", attachment_type="AttachmentType.png")
     # chrome_driver.quit()
 
