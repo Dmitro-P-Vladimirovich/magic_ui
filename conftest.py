b5457c44f6d6475b2a717703b153d1c8f9ef4f5b
Dmitriy Ponomarev <dmitro.p.vladimirovich@gmail.com>
2026-07-21 20:28:09 +0300

    allure attach

--- conftest.py
+++ conftest.py
@@ -17,9 +17,9 @@ def driver():
     chrome_driver = webdriver.Chrome(options=options)
     chrome_driver.maximize_window()
     yield chrome_driver
-    filename = f'{str(random.randint(100, 10000))}.png'
-    chrome_driver.save_screenshot(filename)
-    allure.attach(filename)
+    # filename = f'{str(random.randint(100, 10000))}.png'
+    # chrome_driver.save_screenshot(filename)
+    allure.attach(chrome_driver.get_screenshot_as_png(), name="Screenshot", attachment_type="AttachmentType.PNG")
     chrome_driver.quit()
 
 
